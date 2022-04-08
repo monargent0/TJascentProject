9bf8ea8bf834a57a6c1291b584d98ea73077ef76
jiheok <100196693+jiheok@users.noreply.github.com>
2022-04-08 12:53:56 +0900

    1대1문의 공지사항 뼈대

--- Ascent/src/main/java/com/javalec/ascent/command/ACounselListCommand.java
+++ Ascent/src/main/java/com/javalec/ascent/command/ACounselListCommand.java
@@ -1,5 +1,21 @@
 package com.javalec.ascent.command;
 
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.javalec.ascent.dao.ADaoCounsel;
+import com.javalec.ascent.dto.ADtoC;
+
 public class ACounselListCommand implements ACommand {
 
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+
+		ADaoCounsel daoCounsel = new ADaoCounsel();
+		ArrayList<ADtoC> dtos = daoCounsel.list();
+		request.setAttribute("list", dtos);
+	}
 }
--- Ascent/src/main/java/com/javalec/ascent/command/ACounselWriteCommand.java
+++ Ascent/src/main/java/com/javalec/ascent/command/ACounselWriteCommand.java
@@ -1,5 +1,20 @@
 package com.javalec.ascent.command;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.javalec.ascent.dao.ADaoCounsel;
+
 public class ACounselWriteCommand implements ACommand {
 
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) {
+
+		String counselType = request.getParameter("counselType");
+		String counselTitle = request.getParameter("counselTitle");
+		String counselContent = request.getParameter("counselContent");
+		
+		ADaoCounsel daoCounsel = new ADaoCounsel();
+		daoCounsel.write(counselType, counselTitle, counselContent);
+	}
 }
