9203aea176b2a2494de4327b21364fec704caccf
monargent0 <owjddms1118@gmail.com>
2022-04-08 12:27:34 +0900

    Update ADaoP.java

--- Ascent/src/main/java/com/javalec/ascent/dao/ADaoP.java
+++ Ascent/src/main/java/com/javalec/ascent/dao/ADaoP.java
@@ -1,5 +1,69 @@
 package com.javalec.ascent.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+import com.javalec.ascent.dto.ADtoP;
+
 public class ADaoP {
 
+	DataSource dataSource;
+	
+	public ADaoP() {
+		try {
+			Context context = new InitialContext();
+			dataSource = (DataSource) context.lookup("java:comp/env/jdbc/ascent");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	// 향수종류 - 퍼퓸
+	public ArrayList<ADtoP> ppList() {
+		ArrayList<ADtoP> dtoPs = new ArrayList<ADtoP>();
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = dataSource.getConnection();
+			String query1 = "select productCode,productName,productPrice,productSize,productImages,category_categoryCode from product where productCode like 'P-%'";
+			ps = conn.prepareStatement(query1);
+			rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				String productCode = rs.getString("productCode");
+				String productName = rs.getString("productName");
+				int productPrice = rs.getInt("productPrice");
+				String productSize = rs.getString("productSize");
+				String productImages = rs.getString("productImages");
+				String category_categoryCode = rs.getString("category_categoryCode");
+				
+				ADtoP dtoP = new ADtoP(productCode, productName, productPrice, productSize, productImages, category_categoryCode);
+				dtoPs.add(dtoP);
+				
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(rs != null) rs.close(); //데이터가 있으면 close
+				if(ps!=null) ps.close();
+				if(conn!= null) conn.close();
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return dtoPs;
+		
+	} // 향수종류 - 퍼퓸
+	
 }
