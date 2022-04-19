b352103bcbb1e8a736fda8b0b6b46af92c3711ba
monargent0 <owjddms1118@gmail.com>
2022-04-19 18:08:16 +0900

    spring login

---
+++ Ascent_spring/src/main/java/com/springlec/springAscent/command/LoginCommand.java
@@ -0,0 +1,44 @@
+package com.springlec.springAscent.command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.ui.Model;
+
+import com.springlec.springAscent.dao.UDao;
+import com.springlec.springAscent.dto.UDtoLogin;
+
+
+public class LoginCommand implements ACommand {
+
+	@Override
+	public void execute(HttpServletRequest request, SqlSession sqlSession, Model model) {
+		
+		HttpSession session = request.getSession();
+		String productCode = (String) session.getAttribute("productCode");
+		
+		UDao dao = sqlSession.getMapper(UDao.class);
+		UDtoLogin dto = dao.logIn(request.getParameter("userID"), request.getParameter("userPW"));
+		
+		
+		if(dto == null){
+			session.setAttribute("alertTxt", "일치하는 아이디가 없습니다." );
+			request.setAttribute("ret" , "logInV");
+		} else if ( dto.getAdminCheck().toString().equals("admin")) {
+			session.setAttribute("userID", dto.getUserID().toString() );
+			request.setAttribute("ret", "adminV");			
+		} else if ( dto.getUserID().toString() != null) {
+			session.setAttribute("userID", dto.getUserID().toString() );
+			if(productCode == null){
+				request.setAttribute("ret", "main");
+			}else if(productCode != null){
+				request.setAttribute("ret", "productDetail?productCode="+productCode);
+				session.removeAttribute("productCode");
+				productCode = null;
+			}
+		} 
+
+	}
+
+}
--- Ascent_spring/src/main/java/com/springlec/springAscent/controller/AControllerUser.java
+++ Ascent_spring/src/main/java/com/springlec/springAscent/controller/AControllerUser.java
@@ -1,12 +1,37 @@
 package com.springlec.springAscent.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.springlec.springAscent.command.ACommand;
 
 @Controller
 public class AControllerUser {
 	
 	@Autowired
 	private SqlSession sqlSession;
+	
+	private ACommand loginCommand = null;
+	
+	@Autowired
+	public void auto(ACommand logIn) {
+		this.loginCommand = logIn;
+	}
+	
+	@RequestMapping("/logInV")
+	public String loginV() {
+		return "logInV";
+	}
+	@RequestMapping("/logIn")
+	public String login(HttpServletRequest request, Model model) {
+		loginCommand.execute(request, sqlSession, model);
+		return (String)request.getAttribute("ret");
+//		return "main";
+	}
+	
 }
