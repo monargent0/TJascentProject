c38e42d308ad1d060cb65bdbf5624477466e6138
monargent0 <owjddms1118@gmail.com>
2022-04-20 15:38:39 +0900

    회원가입회원탈퇴

--- Ascent_spring/src/main/java/com/springlec/springAscent/controller/AControllerUser.java
+++ Ascent_spring/src/main/java/com/springlec/springAscent/controller/AControllerUser.java
@@ -16,24 +16,69 @@ public class AControllerUser {
 	private SqlSession sqlSession;
 	
 	private ACommand loginCommand = null;
+	private ACommand signinCommand = null;
+	private ACommand signoutCommand = null;
 	
 	@Autowired
-	public void auto(ACommand logIn) {
+	public void auto(ACommand logIn, ACommand signIn, ACommand signOut) {
 		this.loginCommand = logIn;
+		this.signinCommand = signIn;
+		this.signoutCommand = signOut;
+		
+		
 	}
-	
+	// 임시 메인화면
 	@RequestMapping("/main")
 	public String main() {
 		return "mainV";
 	}
+	// 로그인 화면
 	@RequestMapping("/logInV")
 	public String loginV() {
 		return "logInV";
 	}
+	// 로그인 작동
 	@RequestMapping("/logIn")
 	public String login(HttpServletRequest request) {
 		loginCommand.execute1(sqlSession, request);
 		return (String)request.getAttribute("return");
+	}
+	// 로그아웃
+	@RequestMapping("logOutH")
+	public String logout() {
+		return "logOutH";
+	}
+	// 회원가입 약관 동의
+	@RequestMapping("/signAgreeV")
+	public String signAgree() {
+		return "signAgreeV";
+	}
+	// 회원가입 화면
+	@RequestMapping("/signInV")
+	public String signInV() {
+		return "signInV";
+	}
+	// 회원가입
+	@RequestMapping("/signIn.do")
+	public String signIn(HttpServletRequest request) {
+		signinCommand.execute1(sqlSession, request);
+		return "signInH";
+	}
+	// 회원탈퇴 화면
+	@RequestMapping("signOutV")
+	public String signoutV() {
+		return "signOutV";
+	}
+	// 회원탈퇴
+	@RequestMapping("/signOut.do")
+	public String signOut(HttpServletRequest request) {
+		signoutCommand.execute1(sqlSession, request);
+		return (String)request.getAttribute("return");
+	}
+	// 마이페이지 화면
+	@RequestMapping("/myPageV")
+	public String mypageV() {
+		return "myPageV";
 	}
 	
 }
--- Ascent_spring/src/main/java/com/springlec/springAscent/dao/UDao.java
+++ Ascent_spring/src/main/java/com/springlec/springAscent/dao/UDao.java
@@ -1,10 +1,19 @@
 package com.springlec.springAscent.dao;
 
 
+import org.apache.ibatis.annotations.Param;
+
 import com.springlec.springAscent.dto.UDtoLogin;
 
 
 public interface UDao {
-	public UDtoLogin logIn(String userID, String userPW);
+	// User Page
+	public UDtoLogin logIn(String userID, String userPW); // 로그인
+	public void signin(String userName,String userID,String userEmail,String userPhone,String userPW, String userBirth,String userGender); // 회원가입
+	public void signout(@Param("userID") String userID, @Param("userPW") String userPW); 	// 회원탈퇴
+//	public String findID(String userName, String userEmail);
+//	public String findPW(String userID ,String userName, String userEmail);
+//	public dto userinfo(String userID);
 	
+	// 관리자 Page
 }
