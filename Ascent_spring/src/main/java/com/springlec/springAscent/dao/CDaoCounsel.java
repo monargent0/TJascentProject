11eafc44f8a9bc53864e0084807976cb5d0ba3c3
jiheok <100196693+jiheok@users.noreply.github.com>
2022-04-20 17:31:33 +0900

    CDaoCounsel 기능별 주석

--- Ascent_spring/src/main/java/com/springlec/springAscent/dao/CDaoCounsel.java
+++ Ascent_spring/src/main/java/com/springlec/springAscent/dao/CDaoCounsel.java
@@ -9,12 +9,16 @@ public interface CDaoCounsel {
 	// 1대1 문의 목록 출력
 	public ArrayList<CDtoCounsel> list(String userID);
 	
+	// 1대1 문의 입력
 	public void write(String counselType, String counselTitle, String counselContent, String userID);
 	
+	// 1대1 문의 상세
 	public CDtoCounsel content(int counselCode);
 	
+	// 1대1 문의 삭제
 	public void delete(int counselCode);
 	
+	// 1대1 문의 수정
 	public void modify(String counselType, String counselTitle, String counselContent, int counselCode);
 
 }
