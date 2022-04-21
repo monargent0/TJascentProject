f26e7517acd071a06c01ada9a0561a20da0ac38d
swyoo39 <swyoo39@naver.com>
2022-04-21 19:33:44 +0900

    1234

--- Ascent_spring/src/main/java/com/springlec/springAscent/command/OrderWriteViewCommand.java
+++ Ascent_spring/src/main/java/com/springlec/springAscent/command/OrderWriteViewCommand.java
@@ -25,15 +25,16 @@ public class OrderWriteViewCommand implements ACommand {
 	public void execute3(SqlSession sqlSession, HttpServletRequest request, Model model) {
 		// TODO Auto-generated method stub
 
+		OrderDao dao = sqlSession.getMapper(OrderDao.class);
+		
 		String userID = request.getParameter("userID");
 		String[] checkCart = request.getParameterValues("checkCart");
 		int[] checkCartArray = new int[checkCart.length];
 		// 무조건 1개 이상의 값들만 넘어와서 조건문 필요없음.
-		OrderDao dao = sqlSession.getMapper(OrderDao.class);
 		for(int i=0; i<checkCart.length; i++) {
 			checkCartArray[i] =Integer.parseInt(checkCart[i]);
-			model.addAttribute("orderWriteView", dao.orderWriteView(userID, checkCartArray[i]));
 		}
+		model.addAttribute("orderWriteView", dao.orderWriteView(userID, checkCartArray));
 	}
 
 }
--- Ascent_spring/src/main/java/com/springlec/springAscent/dao/OrderDao.java
+++ Ascent_spring/src/main/java/com/springlec/springAscent/dao/OrderDao.java
@@ -9,7 +9,7 @@ public interface OrderDao {
 
 	public ArrayList<OrderDtoOPC> orderList(String userID);
 	
-	public ArrayList<CartDtoPC> orderWriteView(String userID, int checkCartArray);
+	public ArrayList<CartDtoPC> orderWriteView(String userID, int[] checkCartArray);
 	
 	public void orderWrite(String userID, int cartCodeArray, int orderSum, int orderAmount, 
 			String orderReceiver, String orderPostcode, String orderMainAddress, String orderDetailAddress, 
